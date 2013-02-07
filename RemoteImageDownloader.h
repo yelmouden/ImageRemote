d8405c6655ff0d0d63a491fd0fe54d095670bbb5
yelmouden <yassin.elmouden@gmail.com>
2013-02-07 21:16:35 +0100

    Update RemoteImageDownloader.h

--- RemoteImageDownloader.h
+++ RemoteImageDownloader.h
@@ -2,7 +2,7 @@
 //  RemoteImageDownloader.h
 //  RemoteImages
 //
-//  Created by Vincent GUINIER on 01/02/13.
+//  Created by Yassin El Mouden on 01/02/13.
 //  Copyright (c) 2013 Yassin El Mouden. All rights reserved.
 //
 
