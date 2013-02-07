9fb7afd5e55edfb1e728a1f79bd5afeae30ae892
yelmouden <yassin.elmouden@gmail.com>
2013-02-07 21:16:16 +0100

    Update RemoteCacheManager.m

--- RemoteCacheManager.m
+++ RemoteCacheManager.m
@@ -2,7 +2,7 @@
 //  RemoteCacheManager.m
 //  RemoteImages
 //
-//  Created by Vincent GUINIER on 05/02/13.
+//  Created by Yassin El Mouden on 05/02/13.
 //  Copyright (c) 2013 Yassin El Mouden. All rights reserved.
 //
 
